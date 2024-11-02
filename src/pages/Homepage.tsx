47ad3532dc5bc5205c43912d8b7558d03dd023ca
bryllemutia <bryllemutia3@gmail.com>
2024-11-02 20:23:47 +0800

    initial commit

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CustomDrawer from "../components/CustomDrawer";
+
+const Homepage = () => {
+   return (
+      <div>
+         <div>Homepage</div>
+         <CustomDrawer />
+      </div>
+   );
+};
+
+export default Homepage;

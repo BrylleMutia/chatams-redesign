50740fdc907e5f3558d6486eae2dcec194f01df6
bryllemutia <bryllemutia3@gmail.com>
2024-11-15 04:24:20 +0800

    feature: button grp for severity

---
+++ src/components/SeverityBtnGroup.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+
+import { SEVERITY } from "../constants/drawer";
+
+const SeverityBtnGroup = () => {
+   const [navMode, setNavMode] = useState<string>("p4");
+
+   const handleNavModeChange = (selectedNavMode: string) =>
+      setNavMode(selectedNavMode);
+
+   return (
+      <ButtonGroup
+         variant="outlined"
+         size="small"
+         aria-label="Basic button group"
+      >
+         {SEVERITY.map((sev) => (
+            <Button
+               onClick={() => handleNavModeChange(sev)}
+               variant={navMode === sev ? "contained" : "outlined"}
+            >
+               {sev}
+            </Button>
+         ))}
+      </ButtonGroup>
+   );
+};
+
+export default SeverityBtnGroup;

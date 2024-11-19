bfd83387c03e228a57289070fea0f9f71470b2f9
bryllemutia <bryllemutia3@gmail.com>
2024-11-19 20:44:14 +0800

    feat: note box for displaying notes on items

---
+++ src/components/NoteBox.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import type { Note } from "../constants/sample_notes";
+
+type NoteBoxProps = {
+   noteDetails: Note;
+};
+
+const NoteBox = ({ noteDetails }: NoteBoxProps) => {
+   const { user, date, content } = noteDetails;
+
+   return (
+      <Box
+         sx={{
+            margin: "1em 2em",
+            backgroundColor: "white",
+            padding: "0.5em 1em",
+            borderRadius: "5px",
+            border: "1px solid gray",
+         }}
+      >
+         <Box
+            sx={{
+               display: "flex",
+               justifyContent: "space-between",
+               marginBottom: "0.5em",
+               borderBottom: "1px solid gray",
+               paddingBottom: "0.5em",
+            }}
+         >
+            <Typography variant="caption">{user}</Typography>
+            <Typography variant="caption">{date}</Typography>
+         </Box>
+         <Typography variant="caption">{content}</Typography>
+      </Box>
+   );
+};
+
+export default NoteBox;

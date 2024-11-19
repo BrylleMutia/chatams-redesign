e0bd69fdb6ec7672b4068cec5e3efc0d9c5148b8
bryllemutia <bryllemutia3@gmail.com>
2024-11-19 20:45:14 +0800

    added new components to the custom drawer parent component

--- src/components/CustomDrawer.tsx
+++ src/components/CustomDrawer.tsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
+import { useTheme } from "@mui/material/styles";
 
 import ConstellationLogo from "../assets/constellation.png";
 import CustomDrawerTab from "./CustomDrawerTab";
 import DetailBox from "./DetailBox";
+import NoteBox from "./NoteBox";
+import NoteInput from "./NoteInput";
+import { AppContext } from "../context/AppContext";
+import { AppContextType } from "../context/types";
 
 const drawerWidth = 320;
 
 export default function CustomDrawer() {
+   const { notes } = useContext(AppContext) as AppContextType;
+
+   const theme = useTheme();
+
    return (
       <Box sx={{ display: "flex", height: "100%" }}>
          <CssBaseline />
@@ -57,17 +65,43 @@ export default function CustomDrawer() {
                p: 3,
             }}
          >
-            <Toolbar />
-
             <Box
                sx={{
                   maxWidth: "700px",
-                  border: "1px solid gray",
-                  borderRadius: "15px 15px 0 0",
-                  height: "100%",
+                  border: `1px solid ${theme.palette.grey[500]}`,
+                  borderRadius: "15px",
+                  marginTop: "5%",
+                  backgroundColor: `${theme.palette.grey[500]}`,
                }}
             >
-               <DetailBox />
+               <Box
+                  sx={{
+                     height: "100%",
+                     display: "flex",
+                     flexDirection: "column",
+                     justifyContent: "space-between",
+                  }}
+               >
+                  <Box sx={{ flexGrow: "1" }}>
+                     <DetailBox />
+
+                     <Box
+                        sx={{
+                           overflowY: "scroll",
+                           flexGrow: "1",
+                           maxHeight: "30em",
+                        }}
+                     >
+                        {notes.map((note, index) => (
+                           <NoteBox noteDetails={note} key={index} />
+                        ))}
+                     </Box>
+                  </Box>
+
+                  <Box sx={{ margin: "2em" }}>
+                     <NoteInput />
+                  </Box>
+               </Box>
             </Box>
          </Box>
       </Box>

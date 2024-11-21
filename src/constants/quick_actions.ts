41b7bcc6f22843897318b5a2af6b001e84b8efc7
bryllemutia <bryllemutia3@gmail.com>
2024-11-21 19:31:00 +0800

    added initial quick actions

---
+++ src/constants/quick_actions.ts
@@ -0,0 +1,21 @@
+export type QuickAction = {
+   text: string;
+};
+
+export const QUICK_ACTIONS: QuickAction[] = [
+   {
+      text: "Generate Summary",
+   },
+   {
+      text: "Generate Reply",
+   },
+   {
+      text: "Generate Resolution",
+   },
+   {
+      text: "See Related KBs",
+   },
+   {
+      text: "See Related Incidents",
+   },
+];

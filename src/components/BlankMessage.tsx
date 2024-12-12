8fca137e9d289812d6c49d82a821f2038418e445
bryllemutia <bryllemutia3@gmail.com>
2024-12-12 19:09:42 +0800

    added blank message component

---
+++ src/components/BlankMessage.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const BlankMessage = () => {
+   return (
+      <div>
+         <Typography variant="caption">No items here so far.</Typography>
+      </div>
+   );
+};
+
+export default BlankMessage;

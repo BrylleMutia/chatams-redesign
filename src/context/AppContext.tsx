4cb79ddd12457b7653861e05757f9d7f7dc5d2c4
bryllemutia <bryllemutia3@gmail.com>
2024-12-12 19:13:37 +0800

    added mock data for requests and severity to context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import { createContext } from "react";
-import type { AppContextType, MUITheme } from "./types";
-import type { Incident, Note } from "../constants/sample_inc";
+import type {
+   AppContextType,
+   MUITheme,
+   Incident,
+   Note,
+   Severity,
+   RequestItem,
+   DrawerTabs,
+} from "./types";
 
 export const AppContext = createContext<AppContextType | null>(null);
 
@@ -11,13 +18,18 @@ type AppContextProviderProps = {
 
 const AppContextProvider = ({ children }: AppContextProviderProps) => {
    const [selectedTheme, setSelectedTheme] = useState<MUITheme>("light");
+   const [selectedTab, setSelectedTab] = useState<DrawerTabs>("incident");
    const [isLoading, setIsLoading] = useState<boolean>(false);
    const [selectedItemDetail, setSelectedItemDetail] =
       useState<string>("INC6159676641");
+   const [severityFilter, setSeverityFilter] = useState<Severity>("ALL");
+
+   /** mock data for incidents, please replace with REST API data */
    const [incidents, setIncidents] = useState<Incident[]>([
       {
          number: "INC6159676641",
          state: "new",
+         severity: "P3",
          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
          notes: [
             {
@@ -49,6 +61,7 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
       {
          number: "INC5621251589",
          state: "new",
+         severity: "P3",
          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
          notes: [
             {
@@ -80,6 +93,7 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
       {
          number: "INC4509783111",
          state: "new",
+         severity: "P4",
          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
          notes: [
             {
@@ -111,6 +125,7 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
       {
          number: "INC1662221199",
          state: "inprogress",
+         severity: "P2",
          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
          notes: [
             {
@@ -142,6 +157,7 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
       {
          number: "INC9078456730",
          state: "inprogress",
+         severity: "P3",
          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
          notes: [
             {
@@ -173,6 +189,7 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
       {
          number: "INC4509785111",
          state: "onhold",
+         severity: "P3",
          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
          notes: [
             {
@@ -204,6 +221,7 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
       {
          number: "INC4546711991",
          state: "onhold",
+         severity: "P3",
          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
          notes: [
             {
@@ -235,6 +253,7 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
       {
          number: "INC1122545589",
          state: "resolved",
+         severity: "P4",
          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
          notes: [
             {
@@ -265,8 +284,271 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
       },
    ]);
 
+   /** mock data for requests, please replace with REST API data */
+   const [requests, setRequests] = useState<RequestItem[]>([
+      {
+         number: "RITM7589293144",
+         state: "new",
+         severity: "P4",
+         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
+         notes: [
+            {
+               user: "C673564@constellation.com",
+               date: "10/21/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae odio elementum tempor. Cras eleifend ornare nunc. Nunc fringilla arcu nec bibendum venenatis. Duis tincidunt fringilla justo, sit amet tempus lacus blandit et. ",
+            },
+            {
+               user: "C093564@constellation.com",
+               date: "10/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum imperdiet nisl vel sagittis. Fusce. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas leo nec odio condimentum, sed convallis risus fringilla. Donec nunc. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/20/2024",
+               content:
+                  "Lorem ipsum dolor sit amet consectetur. Mi nibh varius tortor lorem turpis id. Ultricies vitae rhoncus ac aliquam egestas. Pellentesque a libero in et ultricies vitae.",
+            },
+         ],
+      },
+      {
+         number: "RITM5612121135",
+         state: "inprogress",
+         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
+         severity: "P3",
+         notes: [
+            {
+               user: "C954564@constellation.com",
+               date: "10/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum imperdiet nisl vel sagittis. Fusce. ",
+            },
+            {
+               user: "C954564@constellation.com",
+               date: "10/20/2024",
+               content:
+                  "Lorem ipsum dolor sit amet consectetur. Mi nibh varius tortor lorem turpis id. Ultricies vitae rhoncus ac aliquam egestas. Pellentesque a libero in et ultricies vitae.",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/21/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae odio elementum tempor. Cras eleifend ornare nunc. Nunc fringilla arcu nec bibendum venenatis. Duis tincidunt fringilla justo, sit amet tempus lacus blandit et. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas leo nec odio condimentum, sed convallis risus fringilla. Donec nunc. ",
+            },
+         ],
+      },
+      {
+         number: "RITM3576771111",
+         state: "inprogress",
+         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
+         severity: "P3",
+         notes: [
+            {
+               user: "C113564@constellation.com",
+               date: "10/12/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae odio elementum tempor. Cras eleifend ornare nunc. Nunc fringilla arcu nec bibendum venenatis. Duis tincidunt fringilla justo, sit amet tempus lacus blandit et. ",
+            },
+            {
+               user: "C113564@constellation.com",
+               date: "10/17/2024",
+               content:
+                  "Lorem ipsum dolor sit amet consectetur. Mi nibh varius tortor lorem turpis id. Ultricies vitae rhoncus ac aliquam egestas. Pellentesque a libero in et ultricies vitae.",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum imperdiet nisl vel sagittis. Fusce. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas leo nec odio condimentum, sed convallis risus fringilla. Donec nunc. ",
+            },
+         ],
+      },
+      {
+         number: "RITM7990011226",
+         state: "onhold",
+         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
+         severity: "P3",
+         notes: [
+            {
+               user: "C963564@constellation.com",
+               date: "07/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum imperdiet nisl vel sagittis. Fusce. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "07/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas leo nec odio condimentum, sed convallis risus fringilla. Donec nunc. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "07/20/2024",
+               content:
+                  "Lorem ipsum dolor sit amet consectetur. Mi nibh varius tortor lorem turpis id. Ultricies vitae rhoncus ac aliquam egestas. Pellentesque a libero in et ultricies vitae.",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "07/21/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae odio elementum tempor. Cras eleifend ornare nunc. Nunc fringilla arcu nec bibendum venenatis. Duis tincidunt fringilla justo, sit amet tempus lacus blandit et. ",
+            },
+         ],
+      },
+      {
+         number: "RITM7990712894",
+         state: "onhold",
+         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
+         severity: "P2",
+         notes: [
+            {
+               user: "C963564@constellation.com",
+               date: "06/09/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae odio elementum tempor. Cras eleifend ornare nunc. Nunc fringilla arcu nec bibendum venenatis. Duis tincidunt fringilla justo, sit amet tempus lacus blandit et. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "06/09/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum imperdiet nisl vel sagittis. Fusce. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas leo nec odio condimentum, sed convallis risus fringilla. Donec nunc. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/20/2024",
+               content:
+                  "Lorem ipsum dolor sit amet consectetur. Mi nibh varius tortor lorem turpis id. Ultricies vitae rhoncus ac aliquam egestas. Pellentesque a libero in et ultricies vitae.",
+            },
+         ],
+      },
+      {
+         number: "RITM5688010281",
+         state: "resolved",
+         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
+         severity: "P4",
+         notes: [
+            {
+               user: "C613564@constellation.com",
+               date: "08/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum imperdiet nisl vel sagittis. Fusce. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "08/20/2024",
+               content:
+                  "Lorem ipsum dolor sit amet consectetur. Mi nibh varius tortor lorem turpis id. Ultricies vitae rhoncus ac aliquam egestas. Pellentesque a libero in et ultricies vitae.",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "08/21/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae odio elementum tempor. Cras eleifend ornare nunc. Nunc fringilla arcu nec bibendum venenatis. Duis tincidunt fringilla justo, sit amet tempus lacus blandit et. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "08/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas leo nec odio condimentum, sed convallis risus fringilla. Donec nunc. ",
+            },
+         ],
+      },
+      {
+         number: "RITM7098110113",
+         state: "resolved",
+         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
+         severity: "P3",
+         notes: [
+            {
+               user: "C963564@constellation.com",
+               date: "10/20/2024",
+               content:
+                  "Lorem ipsum dolor sit amet consectetur. Mi nibh varius tortor lorem turpis id. Ultricies vitae rhoncus ac aliquam egestas. Pellentesque a libero in et ultricies vitae.",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/21/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae odio elementum tempor. Cras eleifend ornare nunc. Nunc fringilla arcu nec bibendum venenatis. Duis tincidunt fringilla justo, sit amet tempus lacus blandit et. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum imperdiet nisl vel sagittis. Fusce. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "10/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas leo nec odio condimentum, sed convallis risus fringilla. Donec nunc. ",
+            },
+         ],
+      },
+      {
+         number: "RITM9087221108",
+         state: "resolved",
+         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi velit, viverra at libero eget, auctor aliquam purus. Fusce non metus id erat efficitur scelerisque. ",
+         severity: "P3",
+         notes: [
+            {
+               user: "C963564@constellation.com",
+               date: "07/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum imperdiet nisl vel sagittis. Fusce. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "07/23/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas leo nec odio condimentum, sed convallis risus fringilla. Donec nunc. ",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "07/20/2024",
+               content:
+                  "Lorem ipsum dolor sit amet consectetur. Mi nibh varius tortor lorem turpis id. Ultricies vitae rhoncus ac aliquam egestas. Pellentesque a libero in et ultricies vitae.",
+            },
+            {
+               user: "C963564@constellation.com",
+               date: "07/21/2024",
+               content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae odio elementum tempor. Cras eleifend ornare nunc. Nunc fringilla arcu nec bibendum venenatis. Duis tincidunt fringilla justo, sit amet tempus lacus blandit et. ",
+            },
+         ],
+      },
+   ]);
+
    const handleChangeTheme = (theme: MUITheme) => {
       setSelectedTheme(theme);
+   };
+   const handleChangeTab = (tab: DrawerTabs) => {
+      setSelectedTab(tab);
    };
 
    const handleChangeSelectedItemDetail = (itemName: string) => {
@@ -276,7 +558,7 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
    const handleSetIsLoading = (loadingState: boolean) =>
       setIsLoading(loadingState);
 
-   const handleAddNote = (newNote: Note) => {
+   const handleAddIncidentNote = (newNote: Note) => {
       // find selected item then update notes
       const selectedItemIndex = incidents.findIndex(
          (inc) => inc.number === selectedItemDetail
@@ -294,17 +576,44 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
       });
    };
 
+   const handleAddRequestNote = (newNote: Note) => {
+      // find selected item then update notes
+      const selectedItemIndex = requests.findIndex(
+         (req) => req.number === selectedItemDetail
+      );
+
+      setRequests((prev) => {
+         const updatedRequests = [...prev];
+
+         updatedRequests[selectedItemIndex].notes = [
+            ...prev[selectedItemIndex].notes,
+            newNote,
+         ];
+
+         return updatedRequests;
+      });
+   };
+
+   const handleChangeSeverityFilter = (severity: Severity) =>
+      setSeverityFilter(severity);
+
    return (
       <AppContext.Provider
          value={{
             selectedTheme,
             handleChangeTheme,
+            selectedTab,
+            handleChangeTab,
             selectedItemDetail,
             handleChangeSelectedItemDetail,
             isLoading,
             handleSetIsLoading,
             incidents,
-            handleAddNote,
+            handleAddIncidentNote,
+            requests,
+            handleAddRequestNote,
+            severityFilter,
+            handleChangeSeverityFilter,
          }}
       >
          {children}
--- src/context/types.ts
+++ src/context/types.ts
@@ -1,5 +1,3 @@
-import type { Incident, Note } from "../constants/sample_inc";
-
 export type AppContextType = {
    selectedTheme: MUITheme;
    handleChangeTheme: (theme: MUITheme) => void;
@@ -7,8 +5,43 @@ export type AppContextType = {
    handleChangeSelectedItemDetail: (itemName: string) => void;
    isLoading: boolean;
    handleSetIsLoading: (loadingState: boolean) => void;
+   selectedTab: DrawerTabs;
+   handleChangeTab: (tab: DrawerTabs) => void;
    incidents: Incident[];
-   handleAddNote: (note: Note) => void;
+   handleAddIncidentNote: (note: Note) => void;
+   requests: RequestItem[];
+   handleAddRequestNote: (note: Note) => void;
+   severityFilter: Severity;
+   handleChangeSeverityFilter: (severity: Severity) => void;
 };
 
 export type MUITheme = "light" | "dark";
+export type DrawerTabs = "incident" | "request";
+
+/** Incident types */
+export type Incident = {
+   number: string;
+   state: IncidentState;
+   severity: Severity;
+   desc: string;
+   notes: Note[];
+};
+
+export type Note = {
+   user: string;
+   date: string;
+   content: string;
+};
+
+export type IncidentState = "new" | "inprogress" | "onhold" | "resolved";
+export type Severity = "ALL" | "P1" | "P2" | "P3" | "P4";
+
+/** Request types */
+
+export type RequestItem = {
+   number: string;
+   state: IncidentState;
+   severity: Severity;
+   desc: string;
+   notes: Note[];
+};
